984468f1f57c630fad47cc9e06255c570773f93f
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-09-14 16:10:42 +0530

    Internet Check

--- EcomLayoutTask/ViewControllers/Category/CategoryCollectionViewController.swift
+++ EcomLayoutTask/ViewControllers/Category/CategoryCollectionViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import SystemConfiguration
 
 private let reuseIdentifier = "Cell"
 
@@ -35,9 +36,40 @@ class CategoryCollectionViewController: UICollectionViewController {
         flowLayout.itemSize = CGSize(width: dimension, height: height1)
     }
 
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+    func showAlert(_ msg: String) {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     func getCategoryData()
     {
-       
+        guard isInternetAvailable() else {
+            showAlert("No Internet Connection")
+            return
+        }
         let parameters = ["result":["timestamp":""]] as [String:AnyObject]
         Alamofire.request(APIRouter.secondApi(params: parameters )).responseJSON { (responseData) -> Void in
             if let response = (responseData.result.value)  {
--- EcomLayoutTask/ViewControllers/Home/HomeViewController.swift
+++ EcomLayoutTask/ViewControllers/Home/HomeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import SDWebImage
-
+import SystemConfiguration
 
 
 class HomeViewController: UIViewController {
@@ -31,8 +31,40 @@ class HomeViewController: UIViewController {
         getDashboardData()
     }
 
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+    func showAlert(_ msg: String) {
+        let alert = UIAlertController(title: "Alert", message: msg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     func getDashboardData()
     {
+        guard isInternetAvailable() else {
+            showAlert("No Internet Connection")
+            return
+        }
         let Parameters = ["":""]
         Alamofire.request(APIRouter.firstApi(params: Parameters )).responseJSON { (responseData) -> Void in
             if let response = (responseData.result.value)  {
